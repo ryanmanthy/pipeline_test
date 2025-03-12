a41cb10c88a21800e7355d2f3de9ad5a4e7bf0ba
Ryan Manthy <rmanthy@hawk.iit.edu>
2025-03-12 14:51:00 -0700

    fixing scripts

--- quarkus/container/prebuild.sh
+++ quarkus/container/prebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-./mvnw -f dist/pom.xml clean install -DskipTestsuite -DskipExamples -DskipTests
+./mvnw -f distribution/pom.xml clean install -DskipTestsuite -DskipExamples -DskipTests
 cd quarkus
 ./mvnw -f ./pom.xml clean install -DskipTestsuite -DskipExamples -DskipTests
 ./mvnw clean install -DskipTests
