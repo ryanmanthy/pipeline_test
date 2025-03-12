2651a8d72b396b516c78d378da2164eb07ec4774
Ryan Manthy <rmanthy@hawk.iit.edu>
2025-03-12 15:00:33 -0700

    hoping prebuild works

--- .archon/prebuild.sh
+++ .archon/prebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-./mvnw -f distribution/pom.xml clean install -DskipTestsuite -DskipExamples -DskipTests
-cd quarkus
+../mvnw -f ../distribution/pom.xml clean install -DskipTestsuite -DskipExamples -DskipTests
+cd ../quarkus
 ../mvnw -f ./pom.xml clean install -DskipTestsuite -DskipExamples -DskipTests
 ../mvnw clean install -DskipTests
 cd ..
