abdb5fb35c1b68218df8fb97a6eba5e4201c8d68
Ryan Manthy <rmanthy@hawk.iit.edu>
2025-03-12 14:56:07 -0700

    more quarkus bs

--- quarkus/container/prebuild.sh
+++ quarkus/container/prebuild.sh
@@ -2,6 +2,6 @@
 set -e
 ./mvnw -f distribution/pom.xml clean install -DskipTestsuite -DskipExamples -DskipTests
 cd quarkus
-./mvnw -f ./pom.xml clean install -DskipTestsuite -DskipExamples -DskipTests
-./mvnw clean install -DskipTests
+../mvnw -f ./pom.xml clean install -DskipTestsuite -DskipExamples -DskipTests
+../mvnw clean install -DskipTests
 cd ..
